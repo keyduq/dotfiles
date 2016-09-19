8b75dd067cc80c500bcb357ad431cf7aba79b14d
Keyvin Duque <thkeyduq@gmail.com>
2016-09-19 14:17:42 -0400

    agregado script para ver mensajes no leidos en telegram

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # dotfiles
 Mis dotfiles, por los momentos para init.vim (neovim) y .tmux.conf (usando el tema solarized)
 
+El script de tmux-telegram depende de [telegram-cli](https://github.com/vysheng/tg) y [pytg](https://github.com/luckydonald/pytg)
+
 ![Screenshot](https://raw.githubusercontent.com/keyduq/dotfiles/master/screenshot.png)
--- tmux-telegram.py
+++ tmux-telegram.py
@@ -1,2 +1,36 @@
-import dbus;
-bus = dbus.SystemBus()
+#!/usr/bin/env python3
+# Script para obtener los mensajes no leidos de telegram
+# Creado por Keyvin Duque <thkeyduq@gmail.com>
+from pytg.sender import Sender
+import re, subprocess
+
+port = 4458
+
+def findProcess(process):
+    ps= subprocess.Popen('ps -ef | grep ' + process + ' | grep -v grep', shell=True, stdout=subprocess.PIPE)
+    output = ps.stdout.read()
+    ps.stdout.close()
+    ps.wait()
+    return str(output)
+
+def isProcessRunning(process):
+    output = findProcess(process)
+    return re.search(process, output) is not None
+
+if not isProcessRunning('telegram-cli'):
+    subprocess.Popen(['telegram-cli', '-d', '-P', str(port)])
+
+sender = Sender(host='localhost', port=port)
+
+dialog_list = sender.raw('dialog_list')
+
+pattern = r'(?<=\:\s)(?:|[^unread\\])*(?=\s)'
+
+list_unread = re.findall(pattern, dialog_list)
+
+unread = 0
+
+for u in list_unread:
+    unread = unread + int(u)
+
+print("(T) %s msgs sin leer" % unread)
